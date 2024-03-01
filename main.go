9b0b3e7b71edb3ed79646397f5ef490c4a39d1bd
aadzhaan <33817970+aadzhaan@users.noreply.github.com>
2024-03-01 19:10:04 +0700

    auth middleware

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"bwastartup/auth"
 	"bwastartup/handler"
+	"bwastartup/helper"
 	"bwastartup/user"
 	"log"
+	"net/http"
+	"strings"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+// input
+// handler mapping input ke struct
+// service mapping ke struct (ex:User)
+// repository save struct ke db
+// db
 
 func main() {
 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
@@ -31,27 +41,65 @@ func main() {
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
-	api.POST("/avatars", userHandler.UploadAvatar)
+	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
 	router.Run()
 
 }
 
-// func handler(c *gin.Context) {
-// 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+//MIDDLEWARE
+//ambil nilai header Authorization: Bearer token
+//dari header Authorization, kita ambil nilai tokennya saja
+//kita validasi token
+//ambil nilai user_id (yang ada di payload dari jwt)
+//ambil user dari db berdasarkan user_id lewat service
+//kita set context isinya user
 
-// 	if err != nil {
-// 		log.Fatal(err.Error())
-// 	}
+func authMiddleware(authService auth.Service, userService user.Service) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
 
-// 	var users []user.User
-// 	db.Find(&users)
+		if !strings.Contains(authHeader, "Bearer") {
+			response := helper.APIResponse("Unathorized", http.StatusUnauthorized, "error", nil)
+			//menggunakan abort karena middleware ada ditengah2, biar tidak lanjut ke proses selanjutnya jika tidak benar
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
 
-// 	c.JSON(http.StatusOK, users)
-// }
+		//Bearer token
+		var tokenString string
+		arrayToken := strings.Split(authHeader, " ")
+		if len(arrayToken) == 2 {
+			tokenString = arrayToken[1]
+		}
 
-// input
-// handler mapping input ke struct
-// service mapping ke struct (ex:User)
-// repository save struct ke db
-// db
+		token, err := authService.ValidateToken(tokenString)
+		if err != nil {
+			response := helper.APIResponse("Unathorized", http.StatusUnauthorized, "error", nil)
+			//menggunakan abort karena middleware ada ditengah2, biar tidak lanjut ke proses selanjutnya jika tidak benar
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		claim, ok := token.Claims.(jwt.MapClaims)
+
+		if !ok || !token.Valid {
+			response := helper.APIResponse("Unathorized", http.StatusUnauthorized, "error", nil)
+			//menggunakan abort karena middleware ada ditengah2, biar tidak lanjut ke proses selanjutnya jika tidak benar
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		userId := int(claim["user_id"].(float64))
+
+		user, err := userService.GetUserById(userId)
+
+		if err != nil {
+			response := helper.APIResponse("Unathorized", http.StatusUnauthorized, "error", nil)
+			//menggunakan abort karena middleware ada ditengah2, biar tidak lanjut ke proses selanjutnya jika tidak benar
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		c.Set("currentUser", user)
+	}
+}
