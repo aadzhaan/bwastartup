08eab791a578bceb1454f7155d97b2462b596786
aadzhaan <33817970+aadzhaan@users.noreply.github.com>
2024-03-04 20:03:55 +0700

    get campaigns endpoint in progress

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,7 +1,37 @@
 package handler
 
+import (
+	"bwastartup/campaign"
+	"bwastartup/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
 // tangkap parameter di handler
 // handler ke service
 // di service menentukan repository mana yang di-call
 // repository: FindAll(), FindByUserId
 // db
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+// api/v1/campaigns
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userId, _ := strconv.Atoi(c.Query("user_id"))
+	campaigns, err := h.service.GetCampaigns(userId)
+	if err != nil {
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("List of Campaign", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusOK, response)
+}
