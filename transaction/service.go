6c4666a572cc44b3b118119b858e9b7eceb88b83
aadzhaan <33817970+aadzhaan@users.noreply.github.com>
2024-03-11 17:16:23 +0700

    campaign transaction endpoint

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,41 @@
 package transaction
+
+import (
+	"bwastartup/campaign"
+	"errors"
+)
+
+type service struct {
+	repository         Repository
+	campaignRepository campaign.Repository
+}
+
+type Service interface {
+	GetTransactionsByCampaignId(input GetCampaignTransactionInput) ([]Transaction, error)
+}
+
+func NewService(repository Repository, campaignRepository campaign.Repository) *service {
+	return &service{repository, campaignRepository}
+}
+
+func (s *service) GetTransactionsByCampaignId(input GetCampaignTransactionInput) ([]Transaction, error) {
+
+	//get campaign
+	//check campaign.userid != user_id yang melakukan request
+
+	campaign, err := s.campaignRepository.FindByID(input.Id)
+	if err != nil {
+		return []Transaction{}, err
+	}
+
+	if campaign.UserId != input.User.Id {
+		return []Transaction{}, errors.New("Not an owner of the campaign")
+	}
+
+	transactions, err := s.repository.GetByCampaignId(input.Id)
+
+	if err != nil {
+		return transactions, err
+	}
+	return transactions, nil
+}
